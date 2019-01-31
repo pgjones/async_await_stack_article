7825890300f66b747c5a9d58d56160ac88e2670a
pgjones <philip.graham.jones@googlemail.com>
2019-01-31 17:29:46 +0000

    Add the ability to store items on the backend
    This makes a very poor choice to persist (store) the items in a global
    variable. This is bad as the information will be lost whenever the server
    restarts.

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,12 +1,21 @@
-from quart import jsonify, Quart
+from quart import jsonify, Quart, request
+
+items = []
 
 
 app = Quart(__name__)
 
 
-@app.route("/items/")
+@app.route("/items/", methods=["GET"])
 async def get_items():
-    return jsonify(["a", "b", "c"])
+    return jsonify(items)
+
+
+@app.route("/items/", methods=["POST"])
+async def add_item():
+    item = await request.get_json()
+    items.append(item)
+    return jsonify({})
 
 
 if __name__ == "__main__":
